a7b36971602d746ef71a825b688bb8bf4f972753
Suraj <surajsangle00@gmail.com>
2025-05-23 23:02:42 +0530

    update database connection environment variables for MySQL

--- common/dbConnection.js
+++ common/dbConnection.js
@@ -1,10 +1,10 @@
 const sql = require("mysql");
 
 const database = sql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   timezone: "IST",
   options: {
     encrypt: true,
