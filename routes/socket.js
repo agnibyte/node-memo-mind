a43b704919be038a1c0c30808a7229ac69d83f22
Suraj <surajsangle00@gmail.com>
2025-09-22 23:58:53 +0530

    socket connection added

--- routes/socket.js
+++ routes/socket.js
@@ -1,5 +1,7 @@
 const socketController = require("../controllers/socketController");
+const soloGameSocket = require("../controllers/soloGameSocket");
 
 module.exports = (app, io) => {
   socketController(io);
+  soloGameSocket(io);
 };
